b19bc0efc5fd92b546457d09e1ad4636294111dc
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-11-23 14:40:09 -0300

    :bug: fix jojo todynho

--- Client/app.js
+++ Client/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -9,9 +11,6 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
 });
 
 server.listen(3000, () => {
