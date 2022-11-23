211fe8b521ecf880d3bef2ff465d5e24c7915d85
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-11-23 14:38:09 -0300

    jojo todynho

--- Client/app.js
+++ Client/app.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
